e0b13a7778b5fdf486eff869f110e79a594e50f7
LeeMyeongGeon <myeonggeon@gmail.com>
2024-06-23 21:13:26 +0900

    Last

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from "next";
+import React from "react";
+
+import { getPosts, PostType } from "../api";
+import { MainLayout } from "../components/MainLayout";
+import { PostList } from "../components/PostList";
+
+interface Props {
+  posts: PostType[];
+}
+
+const Home: NextPage<Props> = ({ posts }) => (
+  <MainLayout title="홈">
+    <PostList posts={posts} />
+  </MainLayout>
+);
+
+Home.getInitialProps = async () => {
+  const { posts } = await getPosts();
+
+  console.log("executed home getInitialProps");
+
+  return {
+    posts,
+  };
+};
 
 export default Home;
